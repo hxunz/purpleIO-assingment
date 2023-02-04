89b6f9d736471bcba0d34b72adb497bf8cfa6edd
hxunz <guswl2843@gmail.com>
2023-02-04 16:40:52 +0900

    레스토랑 페이지 생성

---
+++ src/pages/restaurants/index.tsx
@@ -0,0 +1,9 @@
+import RestaurantList from '@/components/restaurants/RestaurantList';
+
+const RestaurantsPage = () => {
+  return (
+    <RestaurantList />
+  )
+};
+
+export default RestaurantsPage;
