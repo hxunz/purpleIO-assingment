11c0b5c1bd81300d2890194ffc73685f04f8c288
hxunz <guswl2843@gmail.com>
2023-02-06 13:59:44 +0900

    router 파일 생성

---
+++ src/routes/index.ts
@@ -0,0 +1,14 @@
+export const categories = [
+  {
+    name: 'AWESOME FOOD STORE',
+    path: '/'
+  },
+  {
+    name: 'ABOUT',
+    path: '/about'
+  },
+  {
+    name: 'STORE',
+    path: '/restaurants'
+  }
+];
