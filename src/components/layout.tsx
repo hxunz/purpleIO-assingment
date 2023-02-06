a69f5cd9b2a5208e9e8c4aad6595308b8edf4b66
hxunz <guswl2843@gmail.com>
2023-02-06 14:01:39 +0900

    footer 생성

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,12 +1,14 @@
 import styled from '@emotion/styled';
 import { PropsWithChildren } from 'react';
 import Category from './category/Category'
+import Footer from './footer/Footer';
 
 const Layout = ({ children }: PropsWithChildren) => {
   return (
     <>
       <Category />
       <MainContainer>{children}</MainContainer>
+      <Footer />
     </>
   )
 };
