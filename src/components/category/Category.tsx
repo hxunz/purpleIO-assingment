768166e32466fec8ba6440d4fb0bcf9ef9829c59
hxunz <guswl2843@gmail.com>
2023-02-06 14:00:07 +0900

    카테고리 이름을 누르면 해당 카테고리의 페이지로 이동 현재 위치한 페이지에 따라 카테고리의 이름은 다른 색상으로 변경

--- src/components/category/Category.tsx
+++ src/components/category/Category.tsx
@@ -1,23 +1,28 @@
+import { categories } from '@/routes';
 import styled from '@emotion/styled';
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Link from 'next/link';
-
-const categories = ['AWESOME FOOD STORE', 'ABOUT', 'STORE'];
+import { useRouter } from 'next/router';
 
 const Category = () => {
+  const router = useRouter();
+
   return (
     <>
       <AppBar>
         <CategoryBox>
-          {categories.map((
-            name
-          ) => (
-            <Link href={name.toLowerCase()}>
-              <Categories key={name}>
-                {name}
-              </Categories>
-            </Link>
+          {categories.map(({
+            name,
+            path
+          }) => (
+            <Categories
+              key={name}
+              href={path}
+              isActive={router.pathname === `${path}`}
+            >
+              {name}
+            </Categories>
           ))}
         </CategoryBox>
       </AppBar>
@@ -29,14 +34,15 @@ const CategoryBox = styled(Box)`
   display: flex;
   display: flex;
   align-items: center;
+  background-color: white;
 `
 
-const Categories = styled.div`
-  color: 'white';
+const Categories = styled(Link) <{ isActive: boolean }>`
+  color: ${props => props.isActive ? '#464EA3' : 'black'};
   text-decoration: none;
   font-weight: bold;
-  margin: 1rem 0 1rem 2rem;
   width: 80px;
+  margin: 1rem 0 1rem 2rem;
   overflow-wrap: break-word;
 `
 
