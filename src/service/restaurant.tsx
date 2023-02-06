d6e1f766f96abd1e8008df54566dd1fd7c5cd5f3
hxunz <guswl2843@gmail.com>
2023-02-06 13:59:30 +0900

    불필요한 에러 처리 삭제

--- src/service/restaurant.tsx
+++ src/service/restaurant.tsx
@@ -1,11 +1,7 @@
 import { httpClient } from './api';
 
 export const getRestaurant = async () => {
-  try {
-    const { data } = await httpClient.get('/stores');
+  const { data } = await httpClient.get('/stores');
 
-    return data;
-  } catch (error) {
-    console.error(error);
-  }
+  return data;
 };
